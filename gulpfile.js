809474b7e069c03bc1663ec1d12e666a5b3e0936
James Traver <jtraverjr@gmail.com>
2015-06-08 12:42:55 -0500

    es6 transpiler throwing errors; will fix later

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var es6transpiler = require('gulp-es6-transpiler');
 var settings = {
   customJsFolder: 'js/app/*.js',
   jsLibsFolder: 'js/libs/*.js',
+  jsTranspiledES6Folder: 'js/libs/*.js',
   distributionFolder: 'public/scripts/'
 };
 
@@ -15,8 +16,17 @@ watch([settings.jsLibsFolder, settings.customJsFolder], function() {
 });
 
 gulp.task('default', function () {
-  return gulp.src(['js/libs/jquery.js', 'js/libs/underscore.js', 'js/libs/backbone.js', settings.customJsFolder])
+
+  //gulp.start('transpileES6');
+
+  return gulp.src(['js/libs/jquery.js', 'js/libs/underscore.js', 'js/libs/backbone.js', settings.jsTranspiledES6Folder])
     .pipe(concat('app.js'))
     .pipe(uglify())
+    .pipe(gulp.dest(settings.distributionFolder));
+});
+
+gulp.task('transpileES6', function() {
+  return gulp.src(settings.jsTranspiledES6Folder)
+    .pipe(es6transpiler())
     .pipe(gulp.dest(settings.distributionFolder));
 });
